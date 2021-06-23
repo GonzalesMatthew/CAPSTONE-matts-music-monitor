6b8e0f65747de2c4da24a6037bd3426a023c033c
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-06-22 21:03:10 -0500

    fixed performance issue. had to bring down props to their lowest level of necessity for tasks/setTasks: down from App to where it was finally first needed. also moved firebase initialize app to root index

--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import UserLanding from '../components/UserLanding';
 import TaskWindow from '../components/TaskWindow';
 import ContributionGraph from '../components/ContributionGraph';
+import { getTasks } from '../helpers/data/TaskData';
 
 export default function UserProfile({
-  user, tasks, setTasks
+  user
 }) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    getTasks(user.uid).then(setTasks);
+  }, []);
+
   return (
     <>
       <UserLanding
@@ -28,6 +35,4 @@ export default function UserProfile({
 
 UserProfile.propTypes = {
   user: PropTypes.any,
-  tasks: PropTypes.array,
-  setTasks: PropTypes.func,
 };
